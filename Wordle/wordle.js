e4615ac2c3acf6e191c87983c3f42f1bdbfbce41
Kieran <kieran.webdevelopment@gmail.com>
2022-09-19 15:35:42 +0100

    checking for correct and close items

--- Wordle/wordle.js
+++ Wordle/wordle.js
@@ -1,5 +1,5 @@
 const letters = document.querySelectorAll('.scoreboard-letter');
-const loadingDiv = document.querySelector('info-bar');
+const loadingDiv = document.querySelector('.info-bar');
 const ANSWER_LENGTH = 5;
 
 async function init() {
@@ -27,9 +27,33 @@ async function init() {
     currentRow++;
     currentGuess = '';
   }
+
+  //fetching word
   const res = await fetch('https://words.dev-apis.com/word-of-the-day');
   const resObj = await res.json();
-  const word = resObj.word.toUpperCase;
+  const word = resObj.word.toUpperCase();
+  const wordParts = word.split('');
+  setLoading(false);
+
+  console.log(word);
+
+  const guessParts = currentGuess.split('');
+
+  for (let i = 0; i < ANSWER_LENGTH; i++) {
+    if (guessParts[i] === wordParts[i]) {
+      // mark as correct
+      letters[currentRow * ANSWER_LENGTH + i].classList.add('correct');
+    }
+  }
+  for (let i = 0; i < ANSWER_LENGTH; i++) {
+    if (guessParts[i] === wordParts[i]) {
+      //do nothing
+    } else if (wordParts.includes(guessParts[i])) {
+      letters[currentRow * ANSWER_LENGTH + i].classList.add('close');
+    } else {
+      letters[currentRow * ANSWER_LENGTH + i].classList.add('wrong');
+    }
+  }
 
   function backspace() {
     currentGuess = currentGuess.substring(0, currentGuess.length - 1);
@@ -55,5 +79,9 @@ async function init() {
 
 function isLetter(letter) {
   return /^[a-zA-Z]$/.test(letter);
+}
+
+function setLoading(isLoading) {
+  loadingDiv.classList.toggle('show', isLoading);
 }
 init();
